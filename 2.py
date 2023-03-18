d4f489fbe9d28bc2736192ecbe232e85abe0eebc
sreynen <sreynen130424.gmail>
2023-03-18 13:27:30 +0700

    file Python

---
+++ 2.py
@@ -0,0 +1,7 @@
+ length = float (input ("Enter the length of the room in feet: ")) 
+width = float (input ("Enter the width of the room in feet: ")) 
+   
+area = length * width   
+
+# Display the result 
+print ("The area of the room is", area, "square feet")
